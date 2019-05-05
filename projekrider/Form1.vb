4d13fc9f360466e95a528920afc71358186a76a0
fijar84 <dadangbensin@gmail.com>
2019-05-05 15:29:07 +0700

    v.0.3

--- projekrider/Form1.vb
+++ projekrider/Form1.vb
@@ -1,7 +1,8 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        hello()
+        helloworld()
+
 
     End Sub
 End Class
